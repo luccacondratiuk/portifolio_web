1fd3c7ab6696b3cb78dbc57b196355fe3389dcfd
Jean Lucca Almeida Condratiuk <jlcondratiuk.dev@gmail.com>
2025-08-09 08:21:14 -0300

    feat: add language switcher component

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation';
+import { LanguageSwitcherComponent } from '../language-switcher/language-switcher';
 
 @Component({
   selector: 'app-header',
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, LanguageSwitcherComponent],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
---
+++ src/app/layout/language-switcher/language-switcher.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { I18nService } from '../../i18n/i18n.service';
+
+@Component({
+  selector: 'app-language-switcher',
+  imports: [CommonModule],
+  templateUrl: './language-switcher.html',
+  styleUrl: './language-switcher.scss'
+})
+export class LanguageSwitcherComponent {
+  currentLang: string;
+
+  constructor(private i18nService: I18nService, private router: Router) {
+    this.currentLang = this.i18nService.currentLang;
+  }
+
+  switchLanguage(lang: string): void {
+    if (lang === this.i18nService.currentLang) {
+      return;
+    }
+    this.i18nService.setLanguage(lang);
+    const currentUrl = this.router.url;
+    const pathWithoutLang = currentUrl.replace(/^\/(en|pt-br)/, '');
+    this.router.navigateByUrl(`/${lang}${pathWithoutLang}`);
+    this.currentLang = lang;
+  }
+}
