ccc9fcb31608773176eaf96211961fa59fe03988
Jean Lucca Almeida Condratiuk <jlcondratiuk.dev@gmail.com>
2025-08-15 08:15:52 -0300

    Remove duplicate i18n service and update imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListen
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { I18nModule } from './core/i18n/i18n.module';
-import { LOCALE_ID_PROVIDER } from './core/i18n/i18n.service';
+import { LOCALE_ID_PROVIDER } from './i18n/i18n.service';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
